01349ad465ce4ca0f0debfa1d79f253e0c69b7c0
Bui Kiem <buivantrung1105@gmail.com>
2019-09-18 14:48:00 +0700

    [Endpoint] users using base viewset

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from user.models import User
+from user.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ViewSet):
+    def list(self, request: Request) -> Response:
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True, context={"request": request})
+        return Response(serializer.data)
+
+    def retrieve(self, request: Request, pk: int = None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user, context={"request": request})
+        return Response(serializer.data)
