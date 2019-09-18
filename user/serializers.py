92355bdae147fdb086a7c1dcabf3fb5b4dd8db1a
Bui Kiem <buivantrung1105@gmail.com>
2019-09-18 14:13:02 +0700

    Create User Serializer

---
+++ user/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+from typing import Dict
+
+from user.models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """Serializer for the User model"""
+
+    class Meta:
+        model = get_user_model()
+        fields = ("url", "email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it."""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance: User, validated_data: Dict):
+        """Update the user, setting the password correctly and return it."""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
