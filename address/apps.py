bfcf9d263639c8c0194b73d635ca9c76d0e87552
Bui Kiem <buivantrung1105@gmail.com>
2019-09-20 12:39:22 +0700

    [Signal] Modify raw field before saving on Address

--- address/apps.py
+++ address/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class AddressConfig(AppConfig):
     name = "address"
+
+    def ready(self):
+        import address.signals
---
+++ address/signals.py
@@ -0,0 +1,14 @@
+from typing import Callable
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from .models import Address
+
+
+@receiver(pre_save, sender=Address)
+def modify_raw_field(sender: Address, instance: Address, **kwargs):
+    address_line = instance.address_line
+    district = instance.district.name
+    city = instance.district.city.name
+    country = instance.district.city.country.name
+
+    instance.raw = f"{address_line}, {district}, {city}, {country}"
