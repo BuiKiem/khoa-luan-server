8355aa0abc32ae805bc75e46e94cd6b36bd8c9b1
Bui Kiem <buivantrung1105@gmail.com>
2019-09-22 04:47:44 +0700

    [Admin] Add models to admin site.

--- accommodation/admin.py
+++ accommodation/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from accommodation.models import (
+    AccommodationType,
+    Accommodation,
+    Room,
+    RoomType,
+    Booking,
+)
+
 # Register your models here.
+admin.site.register(AccommodationType)
+admin.site.register(Accommodation)
+admin.site.register(Room)
+admin.site.register(RoomType)
+admin.site.register(Booking)
--- address/admin.py
+++ address/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Country, City, District, Address
+
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(District)
+admin.site.register(Address)
