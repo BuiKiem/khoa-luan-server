c6cc08e589f42c8717db8a45cd8ea93efc4f85d8
Bui Kiem <buivantrung1105@gmail.com>
2019-09-25 22:59:23 +0700

    [View] Query accommodations by location

--- accommodation/views.py
+++ accommodation/views.py
@@ -1,4 +1,9 @@
-from rest_framework import viewsets
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from accommodation.models import (
     AccommodationType,
@@ -14,6 +19,7 @@ from accommodation.serializers import (
     RoomSerializer,
     BookingSerializer,
 )
+from address.models import City, District
 
 
 class AccommodationTypeViewSet(viewsets.ModelViewSet):
@@ -24,6 +30,32 @@ class AccommodationTypeViewSet(viewsets.ModelViewSet):
 class AccommodationViewSet(viewsets.ModelViewSet):
     queryset = Accommodation.objects.all()
     serializer_class = AccommodationSerializer
+
+    @action(methods=["GET"], detail=False, url_path="search")
+    def search_accommodations(self, request: Request):
+        location_type = request.query_params.get("location-type", "")
+        location = request.query_params.get("location", "")
+        selected_city = None
+        selected_district = None
+
+        if not all([location_type, location]):
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"message": "Not provide enough parameter"},
+            )
+
+        if location_type == "city":
+            selected_city = get_object_or_404(City, pk=location)
+        elif location_type == "district":
+            selected_district = get_object_or_404(District, pk=location)
+
+        hotels = Accommodation.objects.filter(
+            Q(address__district=selected_district)
+            | Q(address__district__city=selected_city)
+        )
+        serializer = self.get_serializer(hotels, many=True)
+
+        return Response(serializer.data)
 
 
 class RoomTypeViewSet(viewsets.ModelViewSet):
